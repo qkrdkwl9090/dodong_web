ad5dd701f392211db28b49632ceb614fddaf3273
dodong <qkrdkwl9090@naver.com>
2021-01-10 13:23:51 +0900

    add introduce

--- src/Pages/Intro.js
+++ src/Pages/Intro.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../Scss/Intro.scss';
+import mount from '../images/mount.png';
 
 function Intro(){
     return (
@@ -7,13 +8,14 @@ function Intro(){
             <section className="intro">
                 <div className="titleName">DoDong > 소개</div>
                 <p className="myIntroContent">
-                웹 브라우저를 통해 사람을 연결하고 현실 문제를 해결, 개선하는 매력에 푹 빠져 웹 개발을 길을 걷고 있습니다.<br></br>
+                웹, 어플리케이션을 통해 사람을 연결하고 현실 문제를 해결, 개선하는 매력에 푹 빠져 웹 개발을 길을 걷고 있습니다.<br></br>
                 C#으로 프로그래밍을 시작하여 Javascript의 매력에 푹빠져있고 사회와 코딩을 조금씩 알아가고 있는 사회 초년생입니다.<br></br>
                 문제의식과 해결의 과정으로 성장하고 있으며 항상 새로운 기술에 적극적으로 도전합니다.<br></br><br></br>
-                다양한 개발 스코프를 공부하면서 웹 기획부터 배포·유지보수까지의 과정에 대해 경험해 보았습니다.<br></br>
-                최근에는 프론트엔드에 관심을 가지고 뿌리깊은 개발자를 목표로 노력하고 있습니다.<br></br><br></br>
+                다양한 개발 스코프를 공부하면서 웹, 앱 개발, 서버구축, 유지보수까지의 과정에 대해 경험해 보았습니다.<br></br>
+                최근에는 프론트엔드에 관심을 가지고 전문적인 개발자를 목표로 노력하고 있습니다.<br></br><br></br>
                 반갑습니다. 신뢰를 가장 중요시하는 개발자 박동현입니다. <br></br>
                 </p>
+                <img src={mount}/>
             </section>
         </div>
     )
